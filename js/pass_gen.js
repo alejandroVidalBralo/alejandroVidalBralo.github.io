5f3a5b5c33418103f61a846a0e644895ce058a8b
alejandroVidalBralo <alejandrovidalbralo@gmail.com>
2025-02-19 12:45:18 +0100

    corrected pass_gen

--- js/pass_gen.js
+++ js/pass_gen.js
@@ -1,16 +1,11 @@
 document.getElementById("boton").addEventListener("click", async function() {
-    const palabra1 = document.getElementById("campo1").value;
-    const palabra2 = document.getElementById("campo2").value;
-    const texto = palabra1 + palabra2;
+    const word1 = document.getElementById("campo1").value;
+    const word2 = document.getElementById("campo2").value;
 
-    const hash = await sha256(texto);
+    const hash = await AES(word1, word2);
     document.getElementById("result").value = hash;
 });
 
-async function sha256(text) {
-    const encoder = new TextEncoder();
-    const data = encoder.encode(text);
-    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+async function AES(word1, word2) {
+    return CryptoJS.AES.encrypt(word1, word2).toString();
 }
