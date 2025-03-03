a5a10b5db62de35618cd22eaf3119fc6bbe5ec83
alejandroVidalBralo <alejandrovidalbralo@gmail.com>
2025-03-03 15:24:40 +0100

    changes

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,15 +1,34 @@
-const usefulLink = document.getElementById('useful-link');
-const dropdown = document.getElementById('dropdown');
+const usefulLink1 = document.getElementById('useful-link1');
+const dropdown1 = document.getElementById('dropdown1');
 
-usefulLink.addEventListener('mouseover', () => {
-    usefulLink.innerHTML = '&nbsp;&nbsp;&nbsp;▼&nbsp;&nbsp;&nbsp;'; 
+usefulLink1.addEventListener('mouseover', () => {
+    usefulLink1.innerHTML = '&nbsp;&nbsp;&nbsp;↓ ↓ ↓&nbsp;&nbsp;&nbsp;'; 
 });
 
-usefulLink.addEventListener('mouseout', () => {
-    usefulLink.innerHTML = 'useful for me'; 
+usefulLink1.addEventListener('mouseout', () => {
+    usefulLink1.innerHTML = 'project'; 
 });
 
-usefulLink.addEventListener('click', (e) => {
+usefulLink1.addEventListener('click', (e) => {
     e.preventDefault();
-    dropdown.style.display = dropdown.style.display === 'none' ? 'block' : 'none'; 
+    dropdown1.style.display = dropdown1.style.display === 'none' ? 'block' : 'none'; 
+});
+
+
+
+
+const usefulLink2 = document.getElementById('useful-link2');
+const dropdown2 = document.getElementById('dropdown2');
+
+usefulLink2.addEventListener('mouseover', () => {
+    usefulLink2.innerHTML = '&nbsp;&nbsp;&nbsp;↓ ↓ ↓&nbsp;&nbsp;&nbsp;'; 
+});
+
+usefulLink2.addEventListener('mouseout', () => {
+    usefulLink2.innerHTML = 'useful for me'; 
+});
+
+usefulLink2.addEventListener('click', (e) => {
+    e.preventDefault();
+    dropdown2.style.display = dropdown2.style.display === 'none' ? 'block' : 'none'; 
 });
